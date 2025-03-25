beef48ed1b11ca1be20bb74eec84e484b7da5293
Qwasez1337 <q629536@yandex.ru>
2025-03-25 22:28:50 +0400

    Api Telegram

---
+++ src/main/java/com/taskTelegram/component/BotConfig.java
@@ -0,0 +1,17 @@
+package com.taskTelegram.component;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Configuration
+public class BotConfig {
+    @Bean
+    public TelegramBotsApi telegramBotsApi(FreeTaskManagerBot bot) throws TelegramApiException, TelegramApiException {
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        botsApi.registerBot(bot);
+        return botsApi;
+    }
+}
